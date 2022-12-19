68c247fb960f2ddc3ff12757d31f04fa322467b6
佐藤　陽悦 <y.sato@jc-21.jp>
2022-12-19 15:03:07 +0900

    プレイヤーの移動マップとビットフラグで実現

--- cPlayer.cpp
+++ cPlayer.cpp
@@ -3,29 +3,27 @@
 
 void cPlayer::Input()
 {
-	//0000上下左右
-	//00000001 右
-	//00000010 左
-	//00001010 左上
-	inputFlags = 0;
-	inputFlags = ((KeyUp.pressed()) << 0 ) + ((KeyDown.pressed()) << 1)
-			      + ((KeyLeft.pressed()) << 2) + ((KeyRight.pressed()) << 3);
-}
-
-void cPlayer::Update()
-{
-	Input();
-	//ルックアップテーブルで移動ベクトルをセットしておく
-	std::map<uint8, Vec2> dVec = { {1, { 0,-2 }}, { 2,{0, 2} }, { 4,{-2,0} },
-								   { 5,{-1.2,-1.2} },{6,{-1.2, 1.2}}, { 8,{2,0} },
-                                	{ 9,{1.2,-1.2} },{10,{1.2, 1.2}}};
-									
-	position_ = position_ + dVec[inputFlags];
+	if (KeyUp.pressed())
+	{
+		position_ = position_ + Vec2(0, -2);
+	}
+	if (KeyDown.pressed())
+	{
+		position_ = position_ + Vec2(0, 2);
+	}
+	if (KeyLeft.pressed())
+	{
+		position_ = position_ + Vec2(-2, 0);
+	}
+	if (KeyRight.pressed())
+	{
+		position_ = position_ + Vec2(2, 0);
+	}
 }
 
 void cPlayer::Draw()
 {
 	const double radius = 20.0;
 	//これもパラメータとして設定できるようにしたいね。今のところ定数
-	Circle(position_.x, position_.y, radius).draw(Palette::Aliceblue);
+	Circle(position_.x, position_.y, radius).draw(Palette::Cadetblue);
 }
